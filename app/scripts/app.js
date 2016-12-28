10e5644eb122c27a558068364e41b9e2d32b1183
wuguoyuan <946296536@qq.com>
2016-12-28 22:57:11 +0800

    添加loginjs和无用的依赖

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,7 +9,10 @@
  * Main module of the application.
  */
 var App, modules;
-modules = ['ui.router','ngStorage'];
+modules = ['ui.router', 'ngSanitize', 'ui.bootstrap',
+  'ngTouch', 'ngProgress', 'ui.select', 'checklist-model', "ajoslin.promise-tracker", 'angularPromiseButtons', 'AdminFilters',
+  'AdminService', "ui.bootstrap", "cgBusy", 'ngStorage', 'angular-confirm', 'toastr', 'ngTagsInput', 'naif.base64',
+  'ngUpload', 'ui.tree', 'angularMoment', 'ui.bootstrap.datetimepicker'];
 App = angular.module('daxmallAdmin', modules);
 
 //路由配置
@@ -21,58 +24,58 @@ App.config(['$stateProvider', '$urlRouterProvider',"$locationProvider", function
     templateUrl: 'views/main.html'
   })
     .state('login', {
-      url: '/User/Login',
-      templateUrl: 'views/User/Login.html'
+      url: '/user/login',
+      templateUrl: 'views/user/login.html'
     })
 }]);
 
-////promise 按钮
-//App.config(['angularPromiseButtonsProvider', function(angularPromiseButtonsProvider) {
-//  angularPromiseButtonsProvider.extendConfig({
-//    spinnerTpl: '<i class="fa fa-spinner" aria-hidden="true"></i>',
-//    disableBtn: true,
-//    btnLoadingClass: 'is-loading',
-//    addClassToCurrentBtnOnly: false,
-//    disableCurrentBtnOnly: false
-//  });
-//}]);
-//
-////toast 对话框
-//App.config(['toastrConfig', function(toastrConfig) {
-//  angular.extend(toastrConfig, {
-//    autoDismiss: false,
-//    containerId: 'toast-container',
-//    maxOpened: 0,
-//    timeOut: 3000,
-//    newestOnTop: true,
-//    positionClass: 'toast-top-right',
-//    preventDuplicates: false,
-//    preventOpenDuplicates: false,
-//    target: 'body'
-//  });
-//}]);
-//
+//promise 按钮
+App.config(['angularPromiseButtonsProvider', function(angularPromiseButtonsProvider) {
+  angularPromiseButtonsProvider.extendConfig({
+    spinnerTpl: '<i class="fa fa-spinner" aria-hidden="true"></i>',
+    disableBtn: true,
+    btnLoadingClass: 'is-loading',
+    addClassToCurrentBtnOnly: false,
+    disableCurrentBtnOnly: false
+  });
+}]);
+
+//toast 对话框
+App.config(['toastrConfig', function(toastrConfig) {
+  angular.extend(toastrConfig, {
+    autoDismiss: false,
+    containerId: 'toast-container',
+    maxOpened: 0,
+    timeOut: 3000,
+    newestOnTop: true,
+    positionClass: 'toast-top-right',
+    preventDuplicates: false,
+    preventOpenDuplicates: false,
+    target: 'body'
+  });
+}]);
+
 // http拦截器
-//App.config(['$httpProvider', function($httpProvider) {
-//  $httpProvider.interceptors.push([
-//    '$injector',
-//    function($injector) {
-//      return $injector.get('AuthInterceptor');
-//    }
-//  ]);
-//}]);
-//
-////监控路由变化
-//App.run(['$state', '$rootScope', 'AuthService', function($state, $rootScope, AuthService) {
-//  $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
-//    //判断是否是不包括头部和侧栏的页面
-//    $rootScope.isOwnPage = _.contains(["login"], toState.name);
-//    //路由拦截，无权限则跳转到登录界面
-//    var nextRoute = toState.name;
-//    //console.log(nextRoute)
-//    if (!AuthService.isAuthorized(nextRoute) && !$rootScope.isOwnPage && toState.name != "dashboard") {
-//      event.preventDefault(); //阻止页面跳转\
-//      $state.go('login');
-//    }
-//  });
-//}]);
+App.config(['$httpProvider', function($httpProvider) {
+  $httpProvider.interceptors.push([
+    '$injector',
+    function($injector) {
+      return $injector.get('AuthInterceptor');
+    }
+  ]);
+}]);
+
+//监控路由变化
+App.run(['$state', '$rootScope', 'AuthService', function($state, $rootScope, AuthService) {
+  $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
+    //判断是否是不包括头部和侧栏的页面
+    $rootScope.isOwnPage = _.contains(["login"], toState.name);
+    //路由拦截，无权限则跳转到登录界面
+    var nextRoute = toState.name;
+    //console.log(nextRoute)
+    if (!AuthService.isAuthorized(nextRoute) && !$rootScope.isOwnPage && toState.name != "dashboard") {
+      event.preventDefault(); //阻止页面跳转\
+      $state.go('login');
+    }
+  });
+}]);
--- app/scripts/controllers/user/login.js
+++ app/scripts/controllers/user/login.js
@@ -1,3 +1,39 @@
 /**
  * Created by wugy on 2016/12/28.
  */
+'use strict';
+App.controller('UserLoginController', ['$scope', '$rootScope', '$state', 'AuthService', 'Session', 'msgBus', '$http', 'restful', '$interval',  '$location', function($scope, $rootScope, $state, AuthService, Session, msgBus, $http, restful, $interval,$location) {
+    //初始时将之前登录过的信息清空
+    $scope.load = function() {
+        Session.destroy();
+        //跳转登陆
+        if ($location.search().accessToken) {
+            $scope.credentials.accessToken = $location.search().accessToken;
+            Session.create($location.search().userName);
+            msgBus.emitMsg("login");
+            $state.go('dashboard');
+        }
+    };
+    $scope.credentials = {};
+    $scope.error = "";
+
+    $scope.login = function(credentials) {
+        console.log(credentials);
+        $scope.loginPromise = AuthService.login(credentials).then(function(res) {
+            console.log(res);
+            if (res.code == 1) {
+                console.log(res.msg);
+                return;
+            }
+            if (res.data.userName) {
+                console.log(Session);
+                msgBus.emitMsg("login");
+                $state.go('dashboard');
+            } else {
+                $scope.error = data.msg || "超时";
+            }
+        });
+    };
+
+
+}]);
